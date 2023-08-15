e0e98192b94e18d29d98d647d16602af81a421d1
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2023-08-15 15:51:16 +0530

    Time: 84 ms (21.49%), Space: 42.9 MB (95.60%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,32 +10,29 @@
  */
 class Solution {
     public ListNode mergeLists(ListNode l1,ListNode l2){
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        if(l1.val > l2.val) return mergeLists(l2,l1);
         
-        // if(l1.val > l2.val) return mergeLists(l2,l1);
-        ListNode res = new ListNode(0);
-        ListNode temp = res;
+        ListNode res = l1;
         while(l1!=null && l2!=null){
-            if(l1.val<l2.val){
-                temp.next = l1;
-                temp = temp.next;
+            ListNode temp = null;
+            while(l1!=null && l1.val<=l2.val){
+                temp = l1;
                 l1 = l1.next;
             }
-            else{
-                temp.next = l2;
-                temp = temp.next;
-                l2 = l2.next;
-            }
+            temp.next = l2;
+            
+            ListNode t =l1;
+            l1 = l2;
+            l2 = t;
         }
-        if(l1!=null)temp.next = l1;
-        else temp.next = l2;
-        return res.next;
+        return res;
     }
     public ListNode mergeKLists(ListNode[] lists) {
-        
         int n = lists.length;
         if(n==0) return null;
         int i=1;
-        
         while(i<n){
             // merge two lists
             lists[i] = mergeLists(lists[i],lists[i-1]);
