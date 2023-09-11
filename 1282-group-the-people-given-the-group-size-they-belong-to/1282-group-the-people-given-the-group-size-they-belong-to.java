5cef56377be8ca25bf2dcd7d665ab414d247642d
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2023-09-11 08:52:26 +0530

    Time: 7 ms (55.15%), Space: 44.6 MB (32.96%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,16 +1,16 @@
 class Solution {
+    // Use pen and paper
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         List<List<Integer>> ans = new ArrayList<>();
-        int maxi = groupSizes[0];
-        for(int i:groupSizes) maxi = Math.max(maxi,i);
-        for(int g = 1;g<=maxi;g++){
-            List<Integer> list = new ArrayList<Integer>();
-            for(int i=0;i<groupSizes.length;i++){
-                if(groupSizes[i]==g) list.add(i);
-                if(list.size()==g){
-                    ans.add(list);
-                    list = new ArrayList<Integer>();
-                }
+        HashMap<Integer,List<Integer>> map = new HashMap<>();
+        for(int i=0;i<groupSizes.length;i++){
+            if(!map.containsKey(groupSizes[i])){
+                map.put(groupSizes[i],new ArrayList<>());
+            }
+            map.get(groupSizes[i]).add(i);
+            if(map.get(groupSizes[i]).size()==groupSizes[i]){
+                ans.add(map.get(groupSizes[i]));
+                map.remove(groupSizes[i]);
             }
         }
         return ans;
