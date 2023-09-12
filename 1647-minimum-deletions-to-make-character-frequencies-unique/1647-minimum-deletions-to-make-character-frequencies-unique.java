3c0985ecf1a9397113b4a0b441eb29effe477a26
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2023-09-12 08:50:59 +0530

    Time: 9 ms (75.24%), Space: 45.2 MB (7.31%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -6,8 +6,8 @@ class Solution {
         }
         Arrays.sort(freq);
         int del = 0;
-        for(int i=24;i>=0;i--){
-            if(freq[i]==0) break;
+        for(int i=24;( i>=0 && freq[i]!=0);i--){
+            // if(freq[i]==0) break;
             if(freq[i]>=freq[i+1]){
                 int prev = freq[i];
                 freq[i]=Math.max(0,freq[i+1]-1);
