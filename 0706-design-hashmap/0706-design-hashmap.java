9ba8d1ff07354e440dc2ae57fa8dd07afe87beea
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2023-10-04 14:34:10 +0530

    Time: 14 ms (90.39%), Space: 48.3 MB (60.64%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,19 +1,58 @@
+class ListNode{
+    int key,val;
+    ListNode next;
+    public ListNode(int key,int val,ListNode next){
+        this.key = key;
+        this.val = val;
+        this.next = next;
+    }
+}    
 class MyHashMap {
-    int[] arr;
+    final static int size = 19997;
+    final static int prime = 6291469;
+    ListNode[] data;
+    
     public MyHashMap() {
-        arr = new int[1000001];
-        Arrays.fill(arr,-1);
+        data = new ListNode[size]; 
+    }
+    
+    private int hash(int key){
+        return (int)((long)key * prime % size);
     }
     public void put(int key, int value) {
-        arr[key]=value; 
+        remove(key);
+        int idx = hash(key);
+        ListNode head = data[idx];
+        ListNode newNode = new ListNode(key,value,head);
+        data[idx] = newNode;
     }
     
     public int get(int key) {
-        return arr[key];
+        int idx = hash(key);
+        ListNode head = data[idx];
+        if(head==null) return -1;
+        while(head!=null){
+            if(head.key==key) return head.val;
+            head=head.next;
+        }
+        return -1;   
     }
     
     public void remove(int key) {
-        arr[key]=-1;
+        int idx = hash(key);
+        ListNode head = data[idx];
+        if(head==null) return;
+        if(data[idx].key==key) {
+            data[idx]=data[idx].next;
+            return;
+        }
+        while(head.next!=null){
+            if(head.next.key == key) {
+                head.next = head.next.next;
+                return;
+            }
+            head=head.next;
+        }
     }
 }
 
