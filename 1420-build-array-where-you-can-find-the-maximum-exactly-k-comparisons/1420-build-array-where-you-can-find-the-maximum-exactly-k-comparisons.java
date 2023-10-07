3c5a41165621465467a850284b88f56618648c76
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2023-10-07 10:01:00 +0530

    Time: 62 ms (60.98%), Space: 40.9 MB (85.37%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -22,12 +22,35 @@ class Solution {
         return dp[i][maxi][remain] = ans;
     }
     public int numOfArrays(int n, int m, int k) {
-        int[][][] dp = new int[n][m+1][k+1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= m; j++) {
-                Arrays.fill(dp[i][j], -1);
+        int[][][] dp = new int[n+1][m+1][k+1];
+        
+        for(int i=0;i<=m;i++){
+            dp[n][i][0]=1;
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            for(int maxi=m;maxi>=0;maxi--){
+                for(int remain = 0;remain<=k;remain++){
+                    
+                    //==========
+                    
+                    int ans = 0;
+                    
+                    for(int j=1;j<=maxi;j++){
+                        ans = (ans+dp[i+1][maxi][remain])%MOD;
+                    }
+                    if(remain>0){
+                        for(int j=maxi+1;j<=m;j++){
+                            ans = (ans+dp[i+1][j][remain-1])%MOD;
+                        }
+                    }
+                    dp[i][maxi][remain] = ans;
+                    
+                    //==========
+                    
+                }
             }
         }
-        return f(0,0,k,n,m,dp);
+        return dp[0][0][k];
     }
 }
