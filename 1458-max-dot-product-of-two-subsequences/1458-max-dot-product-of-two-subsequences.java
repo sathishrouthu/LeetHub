98f81ed5fdeba8e71bdf22b3a82185b6bbb4ed25
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2023-10-08 17:09:39 +0530

    Time: 8 ms (98.61%), Space: 43.4 MB (34.72%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -11,6 +11,9 @@ class Solution {
         return dp[i][j] = Math.max(curr,Math.max(nexti,nextj));        
     }
     public int maxDotProduct(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        
         int max1 = nums1[0];
         int min1 = nums1[0];
         int max2 = nums2[0];
@@ -27,7 +30,18 @@ class Solution {
         if(max1<0 && min2>0) return max1*min2;
         if(max2<0 && min1>0) return max2*min1;
         
-        int[][] dp = new int[nums1.length][nums2.length];
-        return f(0,0,nums1,nums2,dp);
+        int[][] dp = new int[n1+1][n2+1];
+        for(int j=0;j<=n2;j++) dp[n1][j]=0;
+        for(int i=0;i<=n1;i++) dp[i][n2]=0;
+        
+        for(int i=n1-1;i>=0;i--){
+            for(int j=n2-1;j>=0;j--){
+                int curr = nums1[i]*nums2[j]+dp[i+1][j+1];
+                int nexti = dp[i+1][j];
+                int nextj = dp[i][j+1];
+                dp[i][j] = Math.max(curr,Math.max(nexti,nextj));
+            }
+        }
+        return dp[0][0];
     }
 }
