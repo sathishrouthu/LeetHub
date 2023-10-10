a432b5050dc7218867da6d3de70d3cb3c1ad410f
Sathish Routhu <93826731+sathishrouthu@users.noreply.github.com>
2023-10-10 09:49:27 +0530

    Time: 51 ms (65.69%), Space: 56.7 MB (71.57%) - LeetHub

---
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[] newArr = new int[n];
+        int idx=0;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            newArr[idx++]=nums[i];
+        }
+        int newArrLen = idx;
+        System.out.println(idx);
+        int ans=n;
+        for(int i=0;i<newArrLen;i++){
+            int left = newArr[i];
+            int right = left+n-1;
+            int j = upperBound(newArr,right,newArrLen);
+            int existingElements = j-i;
+            ans = Math.min(ans,n-existingElements);
+        }
+        return ans;
+    }
+    private int upperBound(int[] arr,int k,int n){
+        int l = 0;
+        int h = n-1;
+        while(l<=h){
+            int mid = (l+h)>>1;
+            if(arr[mid]<=k) l=mid+1;
+            else h=mid-1;
+        }
+        return l;
+    }
+}
